e4eb6e41604bc0dffd091bcb031ba8bd0738f7e2
monika <rajputmonika276@gmail.com>
2021-04-16 16:10:22 +0530

    inital commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// import { without } from 'lodash';
+var _ = require('lodash');
+console.log(_);
+var array = [1,2,3,4,5,6,7,8];
+console.log('answer:', _.without(array, 3));
+// console.log(without);
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+
+console.log(css);
+console.log(color1);
+console.log(color2);
+
